fb68344a97415de4d1ee5d34c38849b7de853459
Steve Crozier <steve@crozier.com>
2014-03-10 20:33:26 -0500

    Render the search page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,11 @@
 class MainController < ApplicationController
 
-  protect_from_forgery except: :developer_callback
-
   def welcome
   end
 
-  def developer_callback
-    @params = params
-  end
-
   def github_callback
-    omni = env['omniauth.auth']
-    @oauth_token = omni['credentials']['token']
+    session[:oauth_token] = env['omniauth.auth']['credentials']['token']
+    render 'search'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Repofinder::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#welcome'
-  post '/auth/developer/callback' => 'main#developer_callback'
   get '/auth/github/callback' => 'main#github_callback'
 
   # Example of regular route:
